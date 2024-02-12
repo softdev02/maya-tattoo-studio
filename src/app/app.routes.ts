15e34b7d6d9838fa167bbad5466e52e277e3c640
softdev02 <developer2.git@gmail.com>
2024-02-12 17:29:43 +0530

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { AdminloginComponent } from './component/adminlogin/adminlogin.component';
 
 export const routes: Routes = [
-    {path:'', component:HomeComponent}
+    {path:'', component:HomeComponent},
+    {path:'admin', component:AdminloginComponent}
 ];
