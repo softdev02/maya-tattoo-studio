765ebc5b490c9462359b8c5e5de01b902f823541
tczsoft <vinayagam.mgb@gmail.com>
2024-02-12 14:38:00 +0530

    new

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from '../../shared/components/hero-section/hero-section.component';
+import { OurServicesComponent } from '../../shared/components/our-services/our-services.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroSectionComponent,OurServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
